fd589bd48d1f96b54c8f406a62eb03fe4221a53c
andrewnoyes <andrew@noyes.io>
2019-09-21 15:01:08 -0600

    ui for sign up/in

--- packages/web/src/app.tsx
+++ packages/web/src/app.tsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import { Switch, Route, RouteComponentProps } from "react-router";
 import { observer, inject } from "mobx-react";
 
-import { AppHeader, Content } from "./components";
+import { AppHeader, Content, UserDialog } from "./components";
 import { Home, GetStarted, NotFound } from "./screens";
 import { AuthStore } from "./stores";
 
@@ -12,20 +12,30 @@ export interface IInjectedProps extends IAppProps {
     authStore: AuthStore;
 }
 
+export interface IAppState {
+    showUserDialog: boolean;
+}
+
 @inject("authStore")
 @observer
-export class App extends Component<IAppProps, {}> {
+export class App extends Component<IAppProps, IAppState> {
+    public state: IAppState = {
+        showUserDialog: false,
+    };
+
     private get injectedProps(): IInjectedProps {
         return this.props as IInjectedProps;
     }
 
     public render() {
         const { authStore } = this.injectedProps;
-        const { isAuthenticated } = authStore;
 
         return (
             <Fragment>
-                <AppHeader isAuthenticated={isAuthenticated} onAuthClick={this.handleAuthClick} />
+                <AppHeader
+                    isAuthenticated={authStore.isAuthenticated}
+                    onAuthClick={this.handleAuthClick}
+                />
                 <main>
                     <Content>
                         <Switch>
@@ -35,12 +45,36 @@ export class App extends Component<IAppProps, {}> {
                         </Switch>
                     </Content>
                 </main>
+                {this.state.showUserDialog && (
+                    <UserDialog
+                        onClose={this.handleCloseDialog}
+                        onSignIn={this.handleSignIn}
+                        onSignUp={this.handleSignUp}
+                    />
+                )}
             </Fragment>
         );
     }
 
     private handleAuthClick = () => {
         const { authStore } = this.injectedProps;
-        authStore.isAuthenticated ? authStore.signOut() : authStore.signIn("", "");
+
+        if (authStore.isAuthenticated) {
+            authStore.signOut();
+        } else {
+            this.handleShowDialog();
+        }
     };
+
+    private handleShowDialog = () => {
+        this.setState({ showUserDialog: true });
+    };
+
+    private handleCloseDialog = () => {
+        this.setState({ showUserDialog: false });
+    };
+
+    private handleSignIn = (email: string, password: string) => {};
+
+    private handleSignUp = (email: string, password: string, displayName: string) => {};
 }
