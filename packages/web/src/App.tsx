d3180793598b172505c3401f0947810bde664ea9
andrewnoyes <andrew@noyes.io>
2019-09-21 12:31:20 -0600

    login button

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,12 +1,19 @@
-import React, { FunctionComponent, Fragment } from "react";
+import React, { FunctionComponent, Fragment, useState } from "react";
 import { Text } from "evergreen-ui";
 
 import { AppHeader, Content } from "./components";
 
 export const App: FunctionComponent = () => {
+    // TODO: this is temporary for auth state
+    const [temp, setTemp] = useState(false);
+
+    const handleAuthClick = () => {
+        setTemp(!temp);
+    };
+
     return (
         <Fragment>
-            <AppHeader />
+            <AppHeader isLoggedIn={temp} onAuthClick={handleAuthClick} />
             <main>
                 <Content>
                     <Text>CONTENT!</Text>
--- packages/web/src/components/app-header.tsx
+++ packages/web/src/components/app-header.tsx
@@ -1,11 +1,18 @@
 import React, { FunctionComponent } from "react";
-import { Pane, Heading } from "evergreen-ui";
+import { Pane, Heading, Button } from "evergreen-ui";
 
-export const AppHeader: FunctionComponent = () => {
+export interface IAppHeaderProps {
+    title?: string;
+    isLoggedIn: boolean;
+    onAuthClick: () => void;
+}
+
+export const AppHeader: FunctionComponent<IAppHeaderProps> = props => {
     return (
         <Pane
             display="flex"
             alignItems="center"
+            justifyContent="space-between"
             height={56}
             width="100%"
             paddingLeft={16}
@@ -15,7 +22,13 @@ export const AppHeader: FunctionComponent = () => {
             zIndex={9}
             top={0}
         >
-            <Heading>Header!</Heading>
+            <Heading>{props.title || "Carpool"}</Heading>
+            <Button
+                appearance={props.isLoggedIn ? "default" : "primary"}
+                onClick={props.onAuthClick}
+            >
+                {props.isLoggedIn ? "Log out" : "Log in"}
+            </Button>
         </Pane>
     );
 };
