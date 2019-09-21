1aa335fbac509e089cb99b5f4a06faa8d22c27a1
andrewnoyes <andrew@noyes.io>
2019-09-21 11:40:43 -0600

    better docker

--- packages/server/src/config/db.config.ts
+++ packages/server/src/config/db.config.ts
@@ -1,30 +1,30 @@
-import { envConfig } from './env.config';
+import { envConfig } from "./env.config";
 const { isProduction, isDevelopment } = envConfig;
 
-const type = 'postgres';
-const host = process.env.CARPOOL_DB_HOST || 'localhost';
-const port = process.env.CARPOOL_DB_PORT || 5433;
-const username = process.env.CARPOOL_DB_USERNAME || 'postgres';
-const password = process.env.CARPOOL_DB_PASSWORD || 'example';
-const database = process.env.CARPOOL_DB_NAME || 'carpool';
-const entities = ['src/**/**.entity{.ts,.js}'];
+const type = "postgres";
+const host = process.env.CARPOOL_DB_HOST || "localhost";
+const port = process.env.CARPOOL_DB_PORT || 5432;
+const username = process.env.CARPOOL_DB_USERNAME || "local";
+const password = process.env.CARPOOL_DB_PASSWORD || "local";
+const database = process.env.CARPOOL_DB_NAME || "carpool";
+const entities = ["src/**/**.entity{.ts,.js}"];
 const synchronize = isDevelopment;
 const url = process.env.DATABASE_URL;
 
 export const dbConfig: any = isProduction
     ? {
-        type,
-        url,
-        synchronize,
-        entities,
-    }
+          type,
+          url,
+          synchronize,
+          entities,
+      }
     : {
-        type,
-        host,
-        port,
-        username,
-        password,
-        database,
-        entities,
-        synchronize,
-    };
+          type,
+          host,
+          port,
+          username,
+          password,
+          database,
+          entities,
+          synchronize,
+      };
