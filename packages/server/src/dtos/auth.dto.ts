eca4999869656b02d7e25fe137a6b2ff623161cd
andrewnoyes <andrew@noyes.io>
2019-09-27 14:58:07 -0600

    min length on password

--- packages/server/src/dtos/auth.dto.ts
+++ packages/server/src/dtos/auth.dto.ts
@@ -1,5 +1,5 @@
 import { ApiModelProperty } from "@nestjs/swagger";
-import { IsEmail } from "class-validator";
+import { IsEmail, MinLength } from "class-validator";
 
 /**
  * AuthDto is used for sign in/up requests. On successful authentication, the UserDto is provided in the response.
@@ -10,5 +10,6 @@ export class AuthDto {
     public readonly email: string;
 
     @ApiModelProperty()
+    @MinLength(8)
     public readonly password: string;
 }
