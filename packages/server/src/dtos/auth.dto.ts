45496700e3b8b180ada18aca349f89b395f7d404
andrewnoyes <andrew@noyes.io>
2019-09-27 14:53:15 -0600

    class validator decorators

--- packages/server/src/dtos/auth.dto.ts
+++ packages/server/src/dtos/auth.dto.ts
@@ -1,10 +1,12 @@
 import { ApiModelProperty } from "@nestjs/swagger";
+import { IsEmail } from "class-validator";
 
 /**
  * AuthDto is used for sign in/up requests. On successful authentication, the UserDto is provided in the response.
  */
 export class AuthDto {
     @ApiModelProperty()
+    @IsEmail()
     public readonly email: string;
 
     @ApiModelProperty()
--- packages/server/src/dtos/sign-up.dto.ts
+++ packages/server/src/dtos/sign-up.dto.ts
@@ -1,10 +1,13 @@
-import { AuthDto } from "./auth.dto";
 import { ApiModelProperty } from "@nestjs/swagger";
+import { Length } from "class-validator";
+
+import { AuthDto } from "./auth.dto";
 
 /**
  * DTO is used for the sign up method.
  */
 export class SignUpDto extends AuthDto {
     @ApiModelProperty()
+    @Length(5, 50)
     public readonly displayName: string;
 }
