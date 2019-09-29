78350daf586dd373f7638b6564c5d8e0f8357b1e
andrewnoyes <andrew@noyes.io>
2019-09-29 14:26:00 -0600

    wrap button in link

--- packages/web/src/screens/create-event.tsx
+++ packages/web/src/screens/create-event.tsx
@@ -8,11 +8,11 @@ export const CreateEvent: FunctionComponent = () => {
     return (
         <Pane margin="auto" width={500} maxWidth="100%" elevation={1} padding={16}>
             <Pane display="flex" alignItems="center">
-                <Button iconBefore="arrow-left" marginRight={36}>
-                    <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+                <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+                    <Button iconBefore="arrow-left" marginRight={36}>
                         Back
-                    </Link>
-                </Button>
+                    </Button>
+                </Link>
                 <Heading>Create Event</Heading>
             </Pane>
             <EventForm />
