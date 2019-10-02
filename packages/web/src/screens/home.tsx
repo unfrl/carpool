783a519549ce98c5c0446a527bc463e02cb852c1
andrewnoyes <andrew@noyes.io>
2019-10-02 13:45:56 -0600

    unused

--- packages/web/src/screens/home.tsx
+++ packages/web/src/screens/home.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent } from "react";
-import { Link } from "react-router-dom";
 import { Fab, makeStyles } from "@material-ui/core";
 
 import { NavLink } from "../components";
