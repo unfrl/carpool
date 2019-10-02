96a7341da4f55c375c177a9696483b7c57f87e6b
andrewnoyes <andrew@noyes.io>
2019-10-02 15:16:09 -0600

    fab ui

--- packages/web/src/screens/home.tsx
+++ packages/web/src/screens/home.tsx
@@ -1,13 +1,45 @@
 import React, { FunctionComponent } from "react";
-import { Fab, makeStyles } from "@material-ui/core";
+import { Typography, Button, makeStyles } from "@material-ui/core";
 
 import { NavLink } from "../components";
+import fastCar from "../images/fast-car.svg";
 
 const useStyles = makeStyles(theme => ({
     root: {
+        left: 0,
+        right: 0,
+        position: "absolute",
+        padding: theme.spacing(2),
+    },
+    hero: {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        flexWrap: "wrap",
+    },
+    info: {
+        [theme.breakpoints.down("sm")]: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+        },
+    },
+    heading: {
+        marginBottom: theme.spacing(1),
+        [theme.breakpoints.down("sm")]: {
+            textAlign: "center",
+        },
+    },
+    subtitle: {
+        marginBottom: theme.spacing(2),
+    },
+    image: {
+        width: "100%",
+        marginTop: theme.spacing(3),
+        [theme.breakpoints.up("sm")]: {
+            maxWidth: "50%",
+            marginTop: 0,
+        },
     },
 }));
 
@@ -16,11 +48,22 @@ export const Home: FunctionComponent = () => {
 
     return (
         <div className={classes.root}>
-            <NavLink to="/create-event">
-                <Fab variant="extended" color="primary" size="large">
-                    Create a Carpool
-                </Fab>
-            </NavLink>
+            <div className={classes.hero}>
+                <div className={classes.info}>
+                    <Typography variant="h3" className={classes.heading}>
+                        Dope header here
+                    </Typography>
+                    <Typography variant="subtitle1" className={classes.subtitle}>
+                        Awesome subtitle with more info here like blehs
+                    </Typography>
+                    <NavLink to="/create-event">
+                        <Button variant="contained" color="primary" size="large">
+                            Create a Carpool
+                        </Button>
+                    </NavLink>
+                </div>
+                <img src={fastCar} alt="Carpool" className={classes.image} />
+            </div>
         </div>
     );
 };
