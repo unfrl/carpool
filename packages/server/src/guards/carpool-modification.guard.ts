e7ff99d05a9661e358a137960d6142601c89a9f6
Matthew Monahan <matt@kaijucode.com>
2019-10-14 14:46:04 -0600

    Carpool auth guard (#64)
    * For now, only the user who created a carpool can modify it.
    
    * Retrieving id from request cleanly (using parameters)

---
+++ packages/server/src/guards/carpool-modification.guard.ts
@@ -0,0 +1,26 @@
+
+import { Injectable, CanActivate, ExecutionContext, NotFoundException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserRequest } from 'src/interfaces';
+
+import { CarpoolService } from "../services";
+
+/*
+This Guard ensures that only the user who created a carpool can modify it.
+*/
+@Injectable()
+export class CarpoolModificationGuard implements CanActivate {
+    constructor(private readonly _carpoolService: CarpoolService) { }
+
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest();
+        return this.validateRequest(request as UserRequest);
+    }
+
+    private async validateRequest(request: UserRequest): Promise<boolean> {
+        const carpool = await this._carpoolService.findOneById(request.params.id);
+        return carpool.createdById === request.user.id;
+    }
+}
