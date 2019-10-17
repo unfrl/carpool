7b24c8f0467bd749e8cac1aa85a154af54759882
Androo <androo@pop-os.localdomain>
2019-10-17 10:54:58 -0600

    verify user display name is not blank and unique

--- packages/server/src/entities/user.entity.ts
+++ packages/server/src/entities/user.entity.ts
@@ -10,7 +10,7 @@ export class User extends BaseEntity {
     @Column({ nullable: true })
     public lastName: string;
 
-    @Column({ length: 50 })
+    @Column({ length: 50, unique: true })
     public displayName: string;
 
     @Column({ unique: true })
