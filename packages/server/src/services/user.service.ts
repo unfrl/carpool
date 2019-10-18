4437f1d2c2abeca5341dfa3bcc7dd142edeaca5d
tbushman <tbushman@pu.bli.sh>
2019-10-18 16:20:54 -0600

    findOne

--- packages/server/src/services/user.service.ts
+++ packages/server/src/services/user.service.ts
@@ -72,6 +72,6 @@ export class UserService {
      * @param displayName - Display name to check exists
      */
     public async displayNameExists(displayName: string): Promise<boolean> {
-        return (await this._userRepository.find({ where: { displayName } })).length > 0;
+        return (await this._userRepository.findOne({ where: { displayName } })) !== undefined;
     }
 }
