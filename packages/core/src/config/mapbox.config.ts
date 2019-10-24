6a8623870fcdb0a8854da0cc4a59a2d2ca1b1c35
Matthew Monahan <matt@kaijucode.com>
2019-10-24 15:31:18 -0600

    forgot about the REACT_APP_* prefix needed to configure react apps

--- packages/core/src/config/mapbox.config.ts
+++ packages/core/src/config/mapbox.config.ts
@@ -1,5 +1,5 @@
 export const mapboxConfig = {
     accessToken:
-        process.env.MAPBOX_ACCESS_TOKEN ||
+        process.env.REACT_APP_MAPBOX_ACCESS_TOKEN ||
         "pk.eyJ1IjoiYW5kcm9vbm9pc2UiLCJhIjoiY2sxankxZjV1MTExdDNpbnlkbXFmMWkyYyJ9.D5kCQlm1iYI_7oHHI1KJkw",
 };
