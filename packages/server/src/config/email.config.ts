af6837d3db63903fec5f10ceefad7fc6473fba3d
matt <matt@kaijucode.com>
2020-12-23 14:07:15 -0700

    Added Maildev to allow us to test email functionallity locally. Updated Readmes (overall & server)

--- packages/server/src/config/email.config.ts
+++ packages/server/src/config/email.config.ts
@@ -1,13 +1,25 @@
-import { HandlebarsAdapter } from "@nest-modules/mailer";
+import { HandlebarsAdapter, MailerOptions } from "@nest-modules/mailer";
 
-const emailHost = process.env.EMAIL_HOST || "smtp-relay.sendinblue.com";
-const emailUser = process.env.EMAIL_USER || "matt@unfrl.com";
-const emailPassword = process.env.EMAIL_PASSWORD || "KgaZA7GVJDcmQbRk";
+const emailHost = process.env.EMAIL_HOST || "localhost";
+const emailPort = process.env.EMAIL_PORT || 1025;
+const emailAuthUser = process.env.EMAIL_AUTH_USER || "unfrl@fakeemail.com";
+const emailPassword = process.env.EMAIL_AUTH_PASSWORD || "";
+const emailTLS = process.env.EMAIL_IGNORE_TLS || true;
+const emailSecure = process.env.EMAIL_SECURE || false;
 
-const transport =
-    process.env.EMAIL_CONNECTION_URL || `smtps://${emailUser}:${emailPassword}@${emailHost}`; //More info on connection urls here: https://nodemailer.com/smtp/ and https://nodemailer.com/usage/using-gmail/
-export const mailModuleConfig: any = {
-    transport,
+export const emailTransport: any = {
+    host: emailHost,
+    port: emailPort,
+    ignoreTLS: emailTLS,
+    secure: emailSecure,
+    auth: {
+        user: emailAuthUser,
+        pass: emailPassword
+    },
+};
+
+export const mailModuleConfig: MailerOptions = {
+    transport: emailTransport,
     defaults: {
         from: "Unfrl LLC <noreply@carpool+unfrl.com>",
     },
@@ -17,18 +29,5 @@ export const mailModuleConfig: any = {
         options: {
             strict: true,
         },
-    },
-};
-
-export const emailConfig: any = {
-    host: emailHost,
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: emailUser,
-        pass: emailPassword,
-    },
-    defaults: {
-        from: "Unfrl LLC <noreply@carpool+unfrl.com>",
     },
 };
